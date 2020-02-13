7de545ec8b084808cd6aafba9602cdfd47d2af58
alihejrati <alihejrati1996@gmail.com>
2020-02-13 22:00:39 +0330

    commit

---
+++ code/source/server/http/services/irantourism/form/insert/service.ts
@@ -0,0 +1,35 @@
+const $config = require('../config.json');
+
+async function service(req, res, next, options: options) {
+    const $code = options['service'].code;
+    const $dbms = $config['dbms'];
+    const $database = $config['database'];
+    const $collection = $config['collection'];
+    const json = {...await crud($dbms, $database, $collection, req, res), ...{
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        sort: Tools.abs(req.body.sort),
+        tag: Tools.isArray(req.body.tag),
+        file: {...Tools.isJson(req.body.file)},
+        flag: { delete: false, hide: false, ...Tools.isJson(req.body.flag) }
+    }};
+
+    try {
+        if (true) {
+            const query = await mongodb.insert($collection, json, { database: $database, errorHandler: error => options['service'].error = error });
+            if (query) {
+                await response.attach(null, req, res);
+            } else {
+                $code(CONFIG['\\status'].notModified);
+                await response.attach(options['service'].error, req, res);
+            }
+        } else {
+            throw 'Not enough information.';
+        }
+    } catch (error) {
+        $code(CONFIG['\\status'].conflict);
+        await response.attach(error, req, res);
+    }
+}
+
+export default service;
